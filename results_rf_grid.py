755549c9d414d61bb81b065c944e4d9ebb50cf86
Jean-François Exbrayat <j.exbrayat@ed.ac.uk>
2018-11-14 15:47:54 +0000

    added script to visualize results of the GridSearch

---
+++ results_rf_grid.py
@@ -0,0 +1,25 @@
+"""
+14/11/2018 - JFE
+This files analyzes the output of the randomized search to create the input for
+the gridsearch
+"""
+
+from useful import *
+from sklearn.externals import joblib
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#load the fitted rf_random
+rf_grid = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/rf_grid.pkl')
+
+# create a pandas dataframe storing parameters and results of the cv
+cv_res = pd.DataFrame(rf_grid.cv_results_['params'])
+params = cv_res.columns #save parameter names for later
+#get the scores as RMSE
+cv_res['mean_train_score'] = .5*(-rf_grid.cv_results_['mean_train_score'])**.5
+cv_res['mean_test_score'] = .5*(-rf_grid.cv_results_['mean_test_score'])**.5
+cv_res['ratio_score'] = cv_res['mean_test_score'] / cv_res['mean_train_score']
+#do some plots
+sns.pairplot(data=cv_res,hue='max_features')
+plt.show()
