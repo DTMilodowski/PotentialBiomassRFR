b7187ea034d87930cc86bf52410b25d3120ac155
Jean-François Exbrayat <j.exbrayat@ed.ac.uk>
2018-11-19 17:53:21 +0000

    added time series variable in output files for SSPs

--- RFR_ssps.py
+++ RFR_ssps.py
@@ -24,6 +24,9 @@ years = np.arange(2015,2101)
 lat = np.arange(90-0.125,-90,-0.25)
 lon = np.arange(-180+0.125,180,0.25)
 
+#get areas
+areas = get_areas()
+
 luh_files = sorted(glob.glob('/disk/scratch/local.2/jexbraya/LUH2/*ssp*'))
 for luh_file in luh_files:
     ssp = luh_file.split('-')[-5]
@@ -46,17 +49,21 @@ for luh_file in luh_files:
             data_vars = {}
             for lvl in ['mean','upper','lower']:
                 data_vars['AGB_'+lvl] = (['time','lat','lon'],np.zeros([years.size,lat.size,lon.size])-9999.,attrs)
+                data_vars['ts_' + lvl] = (['time'],np.zeros(years.size),{'units': 'Pg','long_name': 'total AGB'})
+            agb_ssp = xr.Dataset(data_vars=data_vars,coords=coords)
 
-            agb_hist = xr.Dataset(data_vars=data_vars,coords=coords)
+        agb_ssp.AGB_mean[yy].values[landmask]  = rf_med.predict(X)
+        agb_ssp.AGB_upper[yy].values[landmask] = rf_upp.predict(X)
+        agb_ssp.AGB_lower[yy].values[landmask] = rf_low.predict(X)
 
-        agb_hist.AGB_mean[yy].values[landmask]  = rf_med.predict(X)
-        agb_hist.AGB_upper[yy].values[landmask] = rf_upp.predict(X)
-        agb_hist.AGB_lower[yy].values[landmask] = rf_low.predict(X)
-
+        #get time series
+        agb_ssp.ts_mean[yy] = (agb_ssp.AGB_mean[yy].values*areas)[landmask].sum()*1e-13
+        agb_ssp.ts_upper[yy] = (agb_ssp.AGB_upper[yy].values*areas)[landmask].sum()*1e-13
+        agb_ssp.ts_lower[yy] = (agb_ssp.AGB_lower[yy].values*areas)[landmask].sum()*1e-13
 
     #save to a nc file
     encoding = {'AGB_mean':{'zlib':True,'complevel':1},
                 'AGB_upper':{'zlib':True,'complevel':1},
                 'AGB_lower':{'zlib':True,'complevel':1},}
 
-    agb_hist.to_netcdf('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/output/AGB_%s.nc' % ssp,encoding=encoding)
+    agb_ssp.to_netcdf('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/output/AGB_%s.nc' % ssp,encoding=encoding)
