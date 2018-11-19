c896a0862fcd7917f3c4f4ed5564412a0375d2bf
Jean-François Exbrayat <j.exbrayat@ed.ac.uk>
2018-11-19 17:51:52 +0000

    added time series variable in output file

--- RFR_hist.py
+++ RFR_hist.py
@@ -10,6 +10,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.externals import joblib
 import pandas as pd
 
+
 pca = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/pca_pipeline.pkl')
 
 #load the fitted rfs
@@ -21,6 +22,10 @@ rf_low = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algor
 years = np.arange(1850,2016)
 lat = np.arange(90-0.125,-90,-0.25)
 lon = np.arange(-180+0.125,180,0.25)
+
+#get areas
+areas = get_areas()
+
 for yy, year in enumerate(years):
     predictors,landmask = get_predictors(y0=year)
 
@@ -38,12 +43,18 @@ for yy, year in enumerate(years):
         data_vars = {}
         for lvl in ['mean','upper','lower']:
             data_vars['AGB_'+lvl] = (['time','lat','lon'],np.zeros([years.size,lat.size,lon.size])-9999.,attrs)
+            data_vars['ts_' + lvl] = (['time'],np.zeros(years.size),{'units': 'Pg','long_name': 'total AGB'})
 
         agb_hist = xr.Dataset(data_vars=data_vars,coords=coords)
 
     agb_hist.AGB_mean[yy].values[landmask]  = rf_med.predict(X)
     agb_hist.AGB_upper[yy].values[landmask] = rf_upp.predict(X)
     agb_hist.AGB_lower[yy].values[landmask] = rf_low.predict(X)
+
+    #get time series
+    agb_hist.ts_mean[yy] = (agb_hist.AGB_mean[yy].values*areas)[landmask].sum()*1e-13
+    agb_hist.ts_upper[yy] = (agb_hist.AGB_upper[yy].values*areas)[landmask].sum()*1e-13
+    agb_hist.ts_lower[yy] = (agb_hist.AGB_lower[yy].values*areas)[landmask].sum()*1e-13
 
 
 #save to a nc file
