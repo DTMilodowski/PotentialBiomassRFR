4144dec8f5db211f3397387bd1bd5ebd96391a99
David Milodowski <d.t.milodowski@ed.ac.uk>
2018-11-30 13:36:20 +0000

    results_rf_grid updated with paths etc

--- src/results_rf_grid.py
+++ src/results_rf_grid.py
@@ -11,8 +11,15 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+country_code = sys.argv[1]
+
+path2alg = '/home/dmilodow/DataStore_DTM/FOREST2020/PotentialBiomassRFR/saved_algorithms/'
+path2calval = '/home/dmilodow/DataStore_DTM/FOREST2020/PotentialBiomassRFR/calval/'
+path2data = '/disk/scratch/local.2/dmilodow/PotentialBiomass/processed/%s/' % country_code
+path2agb = path2data+'agb/'
+
 #load the fitted rf_grid
-rf_grid = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/rf_grid.pkl')
+rf_grid = joblib.load('%s%s_rf_grid.pkl' % (path2alg,country_code))
 
 # create a pandas dataframe storing parameters and results of the cv
 cv_res = pd.DataFrame(rf_grid.cv_results_['params'])
@@ -23,13 +30,13 @@ cv_res['mean_test_score'] = .5*(-rf_grid.cv_results_['mean_test_score'])**.5
 cv_res['ratio_score'] = cv_res['mean_test_score'] / cv_res['mean_train_score']
 
 #do some plots
-pca = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/pca_pipeline.pkl')
-predictors,landmask = get_predictors(y0=2000,y1=2009)
+pca = joblib.load('%s%s_pca_pipeline.pkl' % (path2alg,country_code))
+predictors,landmask = get_predictors(country_code)
 
 #transform the data
 X = pca.transform(predictors)
 #get the agb data
-y = xr.open_rasterio('/disk/scratch/local.2/jexbraya/AGB/Avitable_AGB_Map_0.25d.tif')[0].values[landmask]
+y = xr.open_rasterio('%sAvitabile_AGB_%s_1km.tif' % (path2agb,country_code))[0].values[landmask]
 #split train and test subset, specifying random seed
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.25, random_state=26)
 
@@ -57,4 +64,5 @@ for dd, df in enumerate([df_train,df_test]):
 
 #show / save
 fig.show()
+plt.savefig('%s%s_RFgridsearch_calval.png' % (path2calval,country_code))
 #plt.show()
