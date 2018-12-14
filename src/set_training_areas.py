dad1e60665dc3b40b9064a8568c6d78b6f4e3c35
David Milodowski <d.t.milodowski@ed.ac.uk>
2018-12-14 13:36:35 +0000

    function to revise training areas based on previous RFR iteration

--- src/set_training_areas.py
+++ src/set_training_areas.py
@@ -10,27 +10,24 @@ def set(path,subset=1):
     # subset 1: HFL and ESACCI)
     if subset == 1:
         lcfiles = sorted(glob.glob('%s/esacci/*lccs-class*tif' % path))
-        lc = xr.open_rasterio(lcfiles[0]).values
+        lc = xr.open_rasterio(lcfiles[0]).values[0]
         forestmask=np.ones(lc.shape)
         sparsemask=np.ones(lc.shape)
-        print('total number of pixels, N = %i' % forestmask.sum())
-        #sparsemask = (lc>=120)*(lc<130) + (lc>=140)*(lc<160) + (lc>=200)*(lc<210) # shrub, lichen, sparse, bare
-        sparsemask = (lc>=140)*(lc<160) + (lc>=200)*(lc<210)# lichen, sparse, bare
+
+        sparsemask = (lc>=120)*(lc<130) + (lc>=140)*(lc<160) + (lc>=200)*(lc<210) # shrub, lichen, sparse, bare
+        #sparsemask = (lc>=140)*(lc<160) + (lc>=200)*(lc<210)# lichen, sparse, bare
         forestmask =(lc>=50)*(lc<=90) + (lc==160)  + (lc==170)
 
         # loop through years and only keep pixels tha do not change from year to year
         lc_start = 1993
         lc_end = 2015
-        print('training sample, N_forest = %i, N_sparse= %i' % (forestmask.sum(),sparsemask.sum()))
         for ff in range(len(lcfiles)):
             lc_p = lc.copy()
-            lc = xr.open_rasterio(lcfiles[ff]).values
+            lc = xr.open_rasterio(lcfiles[ff]).values[0]
             forestmask *= (lc==lc_p)
             sparsemask *= (lc==lc_p)
-
-
         # load hinterland forests
-        hfl = xr.open_rasterio(glob.glob('%s/forestcover/HFL*tif' % path)[0]).values
+        hfl = xr.open_rasterio(glob.glob('%s/forestcover/HFL*tif' % path)[0]).values[0]
         forestmask=forestmask*(hfl==1)
         # merge masks
         training_mask=sparsemask+forestmask
@@ -38,7 +35,55 @@ def set(path,subset=1):
     # subset 2: HFL only
     elif subset == 2:
         # load hinterland forests
-        hfl = xr.open_rasterio(glob.glob('%s/forestcover/HFL*tif' % path)[0]).values
+        hfl = xr.open_rasterio(glob.glob('%s/forestcover/HFL*tif' % path)[0]).values[0]
         training_mask=hfl==1
 
     return training_mask
+
+# A function that revises the training areas to account for forested areas that
+# are not well represented in parameter space
+# Simple criteria for additional training data:
+# 1) continuous forest (based on ESA-CCI)
+# 2) AGBpot < AGBobs
+# returns both the mask for training, and a coded array indicating whether the
+# training data are part of the original training set (1 = intact forest, 2 =
+# sparse), or are additional forest pixels (3 = additional forest)
+def set_revised(path,AGBobs,AGBpot,landmask):
+
+        lcfiles = sorted(glob.glob('%s/esacci/*lccs-class*tif' % path))
+        lc = xr.open_rasterio(lcfiles[0]).values[0]
+        forestmask=np.ones(lc.shape)
+        sparsemask=np.ones(lc.shape)
+        training_flag=np.zeros(lc.shape)
+
+        sparsemask = (lc>=120)*(lc<130) + (lc>=140)*(lc<160) + (lc>=200)*(lc<210) # shrub, lichen, sparse, bare
+        #sparsemask = (lc>=140)*(lc<160) + (lc>=200)*(lc<210)# lichen, sparse, bare
+        forestmask =(lc>=50)*(lc<=90) + (lc==160)  + (lc==170)
+
+        # loop through years and only keep pixels tha do not change from year to year
+        lc_start = 1993
+        lc_end = 2015
+
+        for ff in range(len(lcfiles)):
+            lc_p = lc.copy()
+            lc = xr.open_rasterio(lcfiles[ff]).values[0]
+            forestmask *= (lc==lc_p)
+            sparsemask *= (lc==lc_p)
+
+        additional_forest = forestmask.copy()
+
+        # load hinterland forests
+        hfl = xr.open_rasterio(glob.glob('%s/forestcover/HFL*tif' % path)[0]).values[0]
+        forestmask_init=forestmask*(hfl==1)
+        forestmask_added = forestmask*(hfl!=1)
+
+        forestmask_added = forestmask_added*(AGBpot<AGBobs)
+
+        training_flag[forestmask_init] = 1
+        training_flag[sparsemask] = 2
+        training_flag[forestmask_added] = 3
+
+        # merge masks
+        training_mask=(sparsemask+forestmask_init+forestmask_added)*landmask
+
+        return training_mask, training_flag
