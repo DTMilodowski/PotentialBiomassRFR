fad71df98ef95e2a32969e107ccd9b82909f5462
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-03-21 15:17:19 +0000

    simple maps update

--- src/simple_figures.py
+++ src/simple_figures.py
@@ -1,9 +1,8 @@
 """
-method_figures
+simple_figures
 --------------------------------------------------------------------------------
 GENERATE FIGURES REQUIRED FOR METHODS DESCRIPTION
-This is a set of scripts to produce images required for methods description in
-slides and papers
+This is a set of scripts to produce simple figures from potential biomass work
 
 21/03/2019 - D. T. Milodowski
 --------------------------------------------------------------------------------
@@ -16,19 +15,19 @@ sns.set()                           # set some nice default plotting options
 
 # Plot simple map figure
 def plot_xarray(xarr, figure_name,figsize_x=8,figsize_y=6,vmin=None,vmax=None,cmap='viridis',add_colorbar=False,
-                extend ='neither',cbar_kwargs={}):
-    if vmin is not None:
+                extend ='neither',cbar_kwargs={},show=False):
+    if vmin is None:
         vmin = np.nanmin(xarr)
-    if vmax is not None:
+    if vmax is None:
         vmax =np.nanmax(xarr)
-
     fig, axis = plt.subplots(nrows=1, ncols=1, figsize=(figsize_x,figsize_y))
     if add_colorbar:
-        xarr.plot(ax=axis, vmin=vmin, vmax=vmax, cmap='viridis', add_colorbar=add_colorbar,
+        xarr.plot(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar,
                     extend=extend, cbar_kwargs=cbar_kwargs)
     else:
-        xarr.plot(ax=axis, vmin=vmin, vmax=vmax, cmap='viridis', add_colorbar=add_colorbar)
+        xarr.plot(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar)
     axis.set_aspect("equal")
     fig.savefig(figure_name)
-    fig.show()
+    if show:
+        fig.show()
     return fig
