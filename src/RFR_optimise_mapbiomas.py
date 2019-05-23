da7fd59dea385fe675f3f08f4262c0af7e9c1990
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-05-23 15:41:40 +0100

    max features

--- src/RFR_optimise_mapbiomas.py
+++ src/RFR_optimise_mapbiomas.py
@@ -88,7 +88,7 @@ n_pca_predictors = Xpca_train.shape[1]
 rf = RandomForestRegressor(criterion="mse",bootstrap=True,n_jobs=-1)
 
 default_params = { "max_depth":scope.int(hp.quniform("max_depth",20,500,1)),              # ***maximum number of branching levels within each tree
-                    "max_features":scope.int(hp.quniform("max_features",int(n_predictors/5),n_predictors+1,1)),      # ***the maximum number of variables used in a given tree
+                    "max_features":scope.int(hp.quniform("max_features",int(n_predictors/5),n_predictors,1)),      # ***the maximum number of variables used in a given tree
                     "min_samples_leaf":scope.int(hp.quniform("min_samples_leaf",1,50,1)),    # ***The minimum number of samples required to be at a leaf node
                     "min_samples_split":scope.int(hp.quniform("min_samples_split",2,200,1)),  # ***The minimum number of samples required to split an internal node
                     "n_estimators":scope.int(hp.quniform("n_estimators",80,120,1)),          # ***Number of trees in the random forest
@@ -96,7 +96,7 @@ default_params = { "max_depth":scope.int(hp.quniform("max_depth",20,500,1)),    
                     "n_jobs":hp.choice("n_jobs",[20,20]) }
 """
 pca_params =      { "max_depth":scope.int(hp.quniform("pca-max_depth",20,500,1)),              # ***maximum number of branching levels within each tree
-                    "max_features":scope.int(hp.quniform("pca-max_features",int(n_pca_predictors/5),n_pca_predictors+1,1)),      # ***the maximum number of variables used in a given tree
+                    "max_features":scope.int(hp.quniform("pca-max_features",int(n_pca_predictors/5),n_pca_predictors,1)),      # ***the maximum number of variables used in a given tree
                     "min_samples_leaf":scope.int(hp.quniform("pca-min_samples_leaf",1,50,1)),    # ***The minimum number of samples required to be at a leaf node
                     "min_samples_split":scope.int(hp.quniform("pca-min_samples_split",2,200,1)),  # ***The minimum number of samples required to split an internal node
                     "n_estimators":scope.int(hp.quniform("pca-n_estimators",80,120,1)),          # ***Number of trees in the random forest
