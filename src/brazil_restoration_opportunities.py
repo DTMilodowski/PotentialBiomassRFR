b8dda2a7a6e1364553ab2456fb5bd7600ff6dd38
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-06-26 14:47:33 +0100

    loading in potential biomass layers

--- src/brazil_restoration_opportunities.py
+++ src/brazil_restoration_opportunities.py
@@ -18,7 +18,6 @@ version = '008'
 #===============================================================================
 PART A: DEFINE PATHS AND LOAD IN DATA
 - Potential biomass maps (from netcdf file)
-- Landcover maps (Mapbiomas)
 - Biome map (Mapbiomas)
 - WRI opportunity map
 #-------------------------------------------------------------------------------
@@ -27,12 +26,13 @@ opportunities_file = '/home/dmilodow/DataStore_DTM/FOREST2020/WRI_restoration_op
 path2data = '/disk/scratch/local.2/dmilodow/PotentialBiomass/processed/%s/' % country_code
 path2output = '/home/dmilodow/DataStore_DTM/FOREST2020/PotentialBiomassRFR/output/'
 path2mapbiomas = '/scratch/local.2/MAPBIOMAS/'
+
 # load potential biomass models from netdf file
-
-
-# load mapbiomas data
-
-
+AGBpot_ds = xr.open_dataset(%s%s_%s_AGB_potential_RFR_worldclim_soilgrids.nc' % (path2output,
+                                country_code,version)
+AGBpot = AGBpot_ds['AGBpot8'].values
+AGBobs = AGBpot_ds['AGBobs'].values
+AGBseq = AGBpot-AGBobs
 # load biome boundaries
 # - for each biome, load in the 1km regridded mapbiomas dataset for that biome
 #   and create a mask based on the pixels for which there is data
@@ -68,14 +68,12 @@ obsC_Mg = np.zeros(4)
 for cc in range(0,4):
     mask = [opportunity.values==cc+1]
     areas_ha[cc] = np.sum(cell_areas[mask])*1.
-    potC_Mg[cc] = np.sum(agbpot_ds[mask]*cell_areas[mask])
-    seqC_Mg[cc] = np.sum(seqpot_ds[mask]*cell_areas[mask])
-    defC_Mg[cc] = np.sum(agbdef_ds[mask]*cell_areas[mask])
-    obsC_Mg[cc] = np.sum(agb_ds[mask]*cell_areas[mask])
+    potC_Mg[cc] = np.sum(AGBpot[mask]*cell_areas[mask])
+    seqC_Mg[cc] = np.sum(AGBseq[mask]*cell_areas[mask])
+    obsC_Mg[cc] = np.sum(AGBobs[mask]*cell_areas[mask])
 
 potC_Mg_ha = potC_Mg/areas_ha
 seqC_Mg_ha = seqC_Mg/areas_ha
-defC_Mg_ha = defC_Mg/areas_ha
 obsC_Mg_ha = obsC_Mg/areas_ha
 
 # opportunity Classes:
@@ -116,23 +114,15 @@ print '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (potC_Mg_ha[0],potC_Mg_ha[1],potC_Mg
 print '====================================================================='
 
 print '====================================================================='
-print ' AGB deficit within each class, in 10^6 Mg C'
+print ' AGB sequestration potential within each class, in 10^6 Mg C'
 print '---------------------------------------------------------------------'
 print '\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture'
-print '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (defC_Mg[0],defC_Mg[1],defC_Mg[2],defC_Mg[3])
+print '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (seqC_Mg[0],seqC_Mg[1],seqC_Mg[2],seqC_Mg[3])
 print '---------------------------------------------------------------------'
 print ' AGB deficit within each class, in 10^6 Mg C / ha'
 print '---------------------------------------------------------------------'
 print '\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture'
-print '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (defC_Mg_ha[0],defC_Mg_ha[1],defC_Mg_ha[2],defC_Mg_ha[3])
-
-
-print '====================================================================='
-print ' Average ratio of deficit to potential agb'
-print '---------------------------------------------------------------------'
-print '\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture'
-print '2005,\t\t%.3f,\t\t%.3f,\t\t%.3f,\t\t%.3f' % (defC_Mg[0]/potC_Mg[0],defC_Mg[1]/potC_Mg[1],defC_Mg[2]/potC_Mg[2],defC_Mg[3]/potC_Mg[3])
-print '====================================================================='
+print '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (seqC_Mg_ha[0],seqC_Mg_ha[1],seqC_Mg_ha[2],seqC_Mg_ha[3])
 
 """
 #===============================================================================
