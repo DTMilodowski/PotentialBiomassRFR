d2914f3368cf8a28474cd3833a195724ff54ae22
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-06-27 15:03:16 +0100

    plotting up summaries

--- src/brazil_restoration_opportunities.py
+++ src/brazil_restoration_opportunities.py
@@ -100,46 +100,95 @@ obsC_Mg_ha = obsC_Mg/areas_ha
 # 3   - Remote restoration
 # 4   - Agricultural lands and urban areas
 print('=====================================================================')
-print('\trestoration opportunity areas in ha')
+print('\trestoration opportunity areas in 10^6 ha')
 print('---------------------------------------------------------------------')
-print('\tforest\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print('\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (areas_ha[0],areas_ha[1],areas_ha[2],areas_ha[3],areas_ha[4]))
+print('\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print('\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (areas_ha[0]/10.**6,
+    areas_ha[1]/10.**6,areas_ha[2]/10.**6,areas_ha[3]/10.**6,areas_ha[4]/10.**6))
 print('=====================================================================')
 
 print('=====================================================================')
 print( '\tobserved biomass within each class, in 10^6 Mg C')
 print( '---------------------------------------------------------------------')
-print( '\tforest\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print( '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (obsC_Mg[0],obsC_Mg[1],obsC_Mg[2],obsC_Mg[3],obsC_Mg[4]))
+print( '\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print( '\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (obsC_Mg[0]/10.**6,
+        obsC_Mg[1]/10.**6,obsC_Mg[2]/10.**6,obsC_Mg[3]/10.**6,obsC_Mg[4]/10.**6))
 print( '---------------------------------------------------------------------')
 print( '\observed biomass density within each class, in 10^6 Mg C / ha')
 print( '---------------------------------------------------------------------')
-print( '\tforest\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print( '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (obsC_Mg_ha[0],obsC_Mg_ha[1],obsC_Mg_ha[2],obsC_Mg_ha[3],obsC_Mg_ha[4]))
+print( '\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print( '\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (obsC_Mg_ha[0],obsC_Mg_ha[1],obsC_Mg_ha[2],obsC_Mg_ha[3],obsC_Mg_ha[4]))
 print( '=====================================================================')
 
 print( '=====================================================================')
 print( '\tpotential biomass within each class, in 10^6 Mg C')
 print( '---------------------------------------------------------------------')
-print( '\tforest\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print( '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (potC_Mg[0],potC_Mg[1],potC_Mg[2],potC_Mg[3],potC_Mg[4]))
+print( '\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print( '\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (potC_Mg[0]/10.**6,
+    potC_Mg[1]/10.**6,potC_Mg[2]/10.**6,potC_Mg[3]/10.**6,potC_Mg[4]/10.**6))
 print( '---------------------------------------------------------------------')
-print( '\tforest\tpotential biomass density within each class, in 10^6 Mg C / ha')
+print( '\tforest\t\tpotential biomass density within each class, in 10^6 Mg C / ha')
 print( '---------------------------------------------------------------------')
-print( '\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print( '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (potC_Mg_ha[0],potC_Mg_ha[1],potC_Mg_ha[2],potC_Mg_ha[3],potC_Mg_ha[4]))
+print( '\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print( '\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (potC_Mg_ha[0],potC_Mg_ha[1],potC_Mg_ha[2],potC_Mg_ha[3],potC_Mg_ha[4]))
 print( '=====================================================================')
 
 print( '=====================================================================')
 print( ' AGB sequestration potential within each class, in 10^6 Mg C')
 print( '---------------------------------------------------------------------')
-print( '\tforest\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print( '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (seqC_Mg[0],seqC_Mg[1],seqC_Mg[2],seqC_Mg[3],seqC_Mg[4]))
+print( '\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print( '\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (seqC_Mg[0]/10.**6,
+    seqC_Mg[1]/10.**6,seqC_Mg[2]/10.**6,seqC_Mg[3]/10.**6,seqC_Mg[4]/10.**6))
 print( '---------------------------------------------------------------------')
 print( ' AGB deficit within each class, in 10^6 Mg C / ha')
 print( '---------------------------------------------------------------------')
-print( '\tforest\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
-print( '\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f,\t\t%.0f' % (seqC_Mg_ha[0],seqC_Mg_ha[1],seqC_Mg_ha[2],seqC_Mg_ha[3],seqC_Mg_ha[4]))
+print( '\tforest\t\twide-scale,\t\tmosaic,\t\tremote,\t\tagriculture')
+print( '\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f,\t\t%.2f' % (seqC_Mg_ha[0],seqC_Mg_ha[1],seqC_Mg_ha[2],seqC_Mg_ha[3],seqC_Mg_ha[4]))
+
+# now plot this information up into some simple charts.
+# Three panels:
+# - Area
+# - AGBobs & AGBpot
+# - AGBseq
+sns.set(style="whitegrid")
+fig,axes = plt.subplots(nrows=1,ncols=3,sharex='all',figsize=[8,3.4])
+sns.barplot(x=opp_class,y=areas_ha,hue=opp_class,palette='Greens_d',dodge=False,
+            ax=axes[0])
+sns.barplot(x=opp_class,y=potC_Mg,hue=opp_class,palette='Greens_d',dodge=False,
+            ax=axes[1],facecolor='white')
+sns.barplot(x=opp_class,y=obsC_Mg,hue=opp_class,palette='Greens_d',dodge=False,
+            ax=axes[1])
+sns.barplot(x=opp_class,y=seqC_Mg,hue=opp_class,palette='Greens_d',dodge=False,
+            ax=axes[2])
+colours=[]
+for patch in axes[0].patches:
+    colours.append(patch.get_facecolor())
+for ax in axes:
+    ax.legend_.remove()
+    ax.set_xticklabels(ax.get_xticklabels(),rotation=45, ha='right')
+    for ii,patch in enumerate(ax.patches):
+        patch.set_edgecolor(colours[ii%len(colours)])
+axes[2].set_ylim(axes[1].get_ylim())
+# convert areas to 10^6 km
+axes[0].set_title('Area of opportunity class')
+y_ticks = axes[0].get_yticks()
+axes[0].set_yticklabels(['{:3.0f}'.format(i/(10**8)) for i in y_ticks])
+axes[0].set_ylabel('Area / 10$^6$ km$^2$')
+# convert Mg to 10^9 Mg
+axes[1].set_title('Aboveground carbon stock')
+axes[1].annotate('potential (open)\nobserved (filled)',xy=(0.95,0.95),
+                xycoords='axes fraction',backgroundcolor='white',ha='right',
+                va='top',fontsize=10)
+y_ticks = axes[1].get_yticks()
+axes[1].set_yticklabels(['{:3.0f}'.format(i/(10**9)) for i in y_ticks])
+axes[1].set_ylabel('Aboveground carbon / 10$^9$ Mg')
+axes[2].set_title('Potential carbon defecit')
+y_ticks = axes[2].get_yticks()
+axes[2].set_yticklabels(['{:3.0f}'.format(i/(10**9)) for i in y_ticks])
+axes[2].set_ylabel('Aboveground carbon / 10$^9$ Mg')
+fig.tight_layout()
+fig.savefig('%s%s_%s_national_summary.png' % (path2output,country_code,version))
+fig.show()
 
 """
 #===============================================================================
@@ -177,3 +226,81 @@ PART D: Breakdown of potential biomass by landcover type for feasible
 restoration areas - not implemented at present
 #-------------------------------------------------------------------------------
 """
+n_biomes = len(biome_labels)
+biome_display_labels = biome_labels.copy()
+biome_display_labels[3] = 'Mata\nAtlantica'
+
+fig,axes = plt.subplots(nrows=n_biomes,ncols=3,sharex='all',sharey='col',
+            figsize=[8,12])
+for ii,biome in enumerate(biome_labels):
+    df_biome=df[df['biome']==biome]
+    sns.barplot(x='opportunity class',y='area_ha',hue='opportunity class',
+                palette='Greens_d',dodge=False,ax=axes[ii][0],
+                data=df_biome)
+    sns.barplot(x='opportunity class',y='AGBpot',hue='opportunity class',
+                palette='Greens_d',dodge=False,facecolor='white',
+                ax=axes[ii][1],data=df_biome)
+    sns.barplot(x='opportunity class',y='AGBobs',hue='opportunity class',
+                palette='Greens_d',dodge=False,ax=axes[ii][1],
+                data=df_biome)
+    sns.barplot(x='opportunity class',y='AGBseq',hue='opportunity class',
+                palette='Greens_d',dodge=False,ax=axes[ii][2],
+                data=df_biome)
+colours=[]
+for patch in axes[0][0].patches:
+    colours.append(patch.get_facecolor())
+for ii,row in enumerate(axes):
+    for jj,ax in enumerate(row):
+        ax.legend_.remove()
+        ax.set_xticklabels(ax.get_xticklabels(),rotation=45, ha='right')
+        ax.set_xlabel(None)
+        for ii,patch in enumerate(ax.patches):
+            patch.set_edgecolor(colours[jj%len(colours)])
+
+#if ii//n_biomes>0:
+#    ax.set_ylim(axes[1].get_ylim())
+
+axes[0][0].set_title('Area of opportunity\nclass / 10$^6$ km$^2$')
+axes[0][1].set_title('Aboveground carbon\nstock / 10$^9$ Mg')
+axes[0][1].annotate('potential (open)\nobserved (filled)',xy=(0.95,0.90),
+                xycoords='axes fraction',backgroundcolor='white',ha='right',
+                va='top',fontsize=10)
+axes[0][2].set_title('Potential carbon\ndefecit / 10$^9$ Mg')
+
+# convert areas to 10^6 km
+for ii,ax in enumerate(axes[:,0]):
+    y_ticks = ax.get_yticks()
+    ax.set_yticklabels(['{:3.0f}'.format(i/(10**8)) for i in y_ticks],fontsize=10)
+    if np.any((ii==1,ii==4)):
+        #ax.set_ylabel('Area / 10$^6$ km$^2$')
+        ax.set_ylabel(None)
+    else:
+        ax.set_ylabel(None)
+
+# convert Mg to 10^9 Mg
+for ii,ax in enumerate(axes[:,1]):
+    y_ticks = ax.get_yticks()
+    ax.set_yticklabels(['{:3.0f}'.format(i/(10**9)) for i in y_ticks],fontsize=10)
+    if np.any((ii==1,ii==4)):
+        #ax.set_ylabel('Aboveground carbon / 10$^9$ Mg')
+        ax.set_ylabel(None)
+    else:
+        ax.set_ylabel(None)
+for ii,ax in enumerate(axes[:,2]):
+    y_ticks = ax.get_yticks()
+    ax.set_yticklabels(['{:3.0f}'.format(i/(10**9)) for i in y_ticks],fontsize=10)
+    if np.any((ii==1,ii==4)):
+        #ax.set_ylabel('Aboveground carbon / 10$^9$ Mg')
+        ax.set_ylabel(None)
+    else:
+        ax.set_ylabel(None)
+
+#fig.tight_layout()
+plt.subplots_adjust(wspace = 0.3,hspace=0.2,left=0.2)
+for ii,ax in enumerate(axes[:,0]):
+    ax.annotate(biome_display_labels[ii],xy=(-.45,0.50),
+                xycoords='axes fraction',backgroundcolor='white',ha='center',
+                va='center')
+
+fig.savefig('%s%s_%s_biome_summary.png' % (path2output,country_code,version))
+fig.show()
