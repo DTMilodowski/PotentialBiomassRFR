81b8eab5b4777db94a71daae9d0b7ad34151c62f
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-08-02 13:05:30 +0100

    updating optimisation routine for run 010; pca and brazil-only

--- src/Brazil/RFR_optimise_mapbiomas.py
+++ src/Brazil/RFR_optimise_mapbiomas.py
@@ -8,6 +8,7 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set()
 
+sys.path.append('../')
 import useful
 import cal_val as cv
 
@@ -24,7 +25,7 @@ from hyperopt.pyll.base import scope
 from functools import partial
 
 country_code = 'BRA'
-version = '008'
+version = '010'
 training_sample_size = 500000
 #load = sys.argv[4]#'new'
 
@@ -56,11 +57,11 @@ pca.fit(predictors_full)
 joblib.dump(pca,'%s/%s_%s_pca_pipeline.pkl' % (path2alg,country_code,version))
 
 # Get additional data masks
-initial_training_mask = useful.get_mask(country_code,mask_def=5)
+initial_training_mask = useful.get_mask(country_code,mask_def=7)
 initial_training_mask[mapbiomas==0]=0
 
 # get subset of predictors for initial training set
-X = predictors_full[initial_training_mask[landmask]]
+X = pca.transform(predictors_full[initial_training_mask[landmask]])
 y = yall[initial_training_mask[landmask]]
 lc = mapbiomas[landmask][initial_training_mask[landmask]]
 
@@ -83,49 +84,22 @@ for idx_train, idx_test in sss.split(X,lc):
     X_train, X_test = X[idx_train], X[idx_test]
     y_train, y_test = y[idx_train], y[idx_test]
     lc_train, lc_test = lc[idx_train], lc[idx_test]
-# create pca transform for testing against RF performance
-Xpca_train = pca.transform(X_train)
 
 n_predictors = X_train.shape[1]
-n_pca_predictors = Xpca_train.shape[1]
 
 # set up hyperparameterspace for optimisation
 rf = RandomForestRegressor(criterion="mse",bootstrap=True,n_jobs=-1,n_estimators=80)
 
 #min_samples_split_iter = hp.quniform("min_samples_split",2,200,1)
 #min_samples_leaf_iter = hp.quniform("min_samples_leaf",1,min_samples_split_iter,1)
-"""
-default_params = { "max_depth":scope.int(hp.quniform("max_depth",20,500,1)),              # ***maximum number of branching levels within each tree
-                    "max_features":scope.int(hp.quniform("max_features",int(n_predictors/5),n_predictors,1)),      # ***the maximum number of variables used in a given tree
-                    "min_samples_leaf":scope.int(hp.quniform("min_samples_leaf",1,50,1)),    # ***The minimum number of samples required to be at a leaf node
-                    "min_samples_split":scope.int(hp.quniform("min_samples_split",2,200,1)),  # ***The minimum number of samples required to split an internal node
-                    "n_estimators":scope.int(hp.quniform("n_estimators",80,120,1)),          # ***Number of trees in the random forest
-                    "min_impurity_decrease":hp.uniform("min_impurity_decrease",0.0,0.2),
-                    "n_jobs":hp.choice("n_jobs",[20,20]) }
-"""
-default_params = { "max_depth":scope.int(hp.quniform("max_depth",20,500,1)),              # ***maximum number of branching levels within each tree
+param_space = { "max_depth":scope.int(hp.quniform("max_depth",20,400,1)),              # ***maximum number of branching levels within each tree
                     "max_features":scope.int(hp.quniform("max_features",int(n_predictors/4),n_predictors,1)),      # ***the maximum number of variables used in a given tree
-                    "min_samples_leaf":scope.int(hp.quniform("min_samples_leaf",1,15,1)),    # ***The minimum number of samples required to be at a leaf node
-                    "min_samples_split":scope.int(hp.quniform("min_samples_split",2,50,1)),  # ***The minimum number of samples required to split an internal node
-                    "n_estimators":scope.int(hp.quniform("n_estimators",80,120,1)),          # ***Number of trees in the random forest
-                    "min_impurity_decrease":hp.uniform("min_impurity_decrease",0.0,0.05),
+                    "min_samples_leaf":scope.int(hp.quniform("min_samples_leaf",2,15,1)),    # ***The minimum number of samples required to be at a leaf node
+                    "min_samples_split":scope.int(hp.quniform("min_samples_split",3,50,1)),  # ***The minimum number of samples required to split an internal node
+                    "n_estimators":scope.int(hp.quniform("n_estimators",100,150,1)),          # ***Number of trees in the random forest
+                    "min_impurity_decrease":hp.uniform("min_impurity_decrease",0.0,0.02),
                     "n_jobs":hp.choice("n_jobs",[20,20]) }
 
-pca_params =      { "max_depth":scope.int(hp.quniform("pca-max_depth",20,500,1)),              # ***maximum number of branching levels within each tree
-                    "max_features":scope.int(hp.quniform("pca-max_features",int(n_pca_predictors/5),n_pca_predictors,1)),      # ***the maximum number of variables used in a given tree
-                    "min_samples_leaf":scope.int(hp.quniform("pca-min_samples_leaf",1,50,1)),    # ***The minimum number of samples required to be at a leaf node
-                    "min_samples_split":scope.int(hp.quniform("pca-min_samples_split",2,200,1)),  # ***The minimum number of samples required to split an internal node
-                    "n_estimators":scope.int(hp.quniform("pca-n_estimators",60,80,1)),          # ***Number of trees in the random forest
-                    "min_impurity_decrease":hp.uniform("pca-min_impurity_decrease",0.0,0.2),
-                    "n_jobs":hp.choice("pca-n_jobs",[20,20]) }
-"""
-space = hp.choice('version',[{'preprocessing':'none',
-                                    'params': default_params
-                                    },
-                                    {'preprocessing':'pca',
-                                    'params':pca_params
-                                    }])
-"""
 # define a function to quantify the objective function
 def f(params):
     global best_score
@@ -164,8 +138,8 @@ def f(params):
 # - number of sampled candidates to calculate expected improvement (n_EI_candidates)
 trials=Trials()
 #trials=pickle.load(open('%s/%s_%s_rf_hyperopt_trials' % (path2alg,country_code,version), "rb"))
-max_evals_target = 180
-spin_up_target = 60
+max_evals_target = 200
+spin_up_target = 50
 best_score = -np.inf
 seed=0
 fail_count=0
@@ -173,25 +147,25 @@ fail_count=0
 # Start with randomised search - setting this explicitly to account for some
 # iterations not being accepted
 print("Starting randomised search (spin up)")
-best = fmin(f, default_params, algo=rand.suggest, max_evals=spin_up_target, trials=trials)
+best = fmin(f, param_space, algo=rand.suggest, max_evals=spin_up_target, trials=trials)
 spin_up = spin_up_target+fail_count
 while (len(trials.trials)-fail_count)<spin_up_target:
     print('\tTarget: %i; iterations: %i; failures: %i' % (spin_up_target,len(trials.trials),fail_count))
     spin_up+=1
-    best = fmin(f, default_params, algo=rand.suggest, max_evals=spin_up, trials=trials)
+    best = fmin(f, param_space, algo=rand.suggest, max_evals=spin_up, trials=trials)
 
 # Now do the TPE search
 print("Starting TPE search")
 max_evals = max_evals_target+fail_count
-algorithm = partial(tpe.suggest, n_startup_jobs=spin_up, gamma=0.25, n_EI_candidates=24)
-best = fmin(f, default_params, algo=algorithm, max_evals=max_evals, trials=trials)
+algorithm = partial(tpe.suggest, n_startup_jobs=spin_up, gamma=0.15, n_EI_candidates=50)
+best = fmin(f, param_space, algo=algorithm, max_evals=max_evals, trials=trials)
 # Not every hyperparameter set will be accepted, so need to conitnue searching
 # until the required number of evaluations is met
 max_evals = max_evals_target+fail_count
 while (len(trials.trials)-fail_count)<max_evals_target:
     print('\tTarget: %i; iterations: %i; failures: %i' % (max_evals_target,len(trials.trials),fail_count))
     max_evals+=1
-    best = fmin(f, default_params, algo=algorithm, max_evals=max_evals, trials=trials)
+    best = fmin(f, param_space, algo=algorithm, max_evals=max_evals, trials=trials)
 
 print('\n\n%i iterations, from which %i failed' % (max_evals,fail_count))
 print('best:')
