b9a76329ba8657c06f46595c56b2a8a8325e0c5b
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-12-10 16:21:40 +0000

    correct hinterland map

--- src/regrid_remote_sensing_data.py
+++ src/regrid_remote_sensing_data.py
@@ -27,20 +27,20 @@ import os
 import glob
 
 # Bounding Box
-"""
+
 W = 92.
 E = 154.
 N = 11.
-S = -21.
-"""
-W = -86.
-E = -34.
-N = 15.
-S = -20.
+S = -12.
+
+#W = -86.
+#E = -34.
+#N = 15.
+#S = -20.
 # create some directories to host the processed data_io
-#prefix = 'INDO'
-prefix = 'AMZ'
-outdir = '/disk/scratch/local.2/dmilodow/PotentialBiomass/processed/'
+prefix = 'INDO'
+#prefix = 'AMZ'
+outdir = '/disk/scratch/local.2/PotentialBiomass/processed/'
 os.system('mkdir %s' % outdir)
 os.system('mkdir %s%s' % (outdir,prefix))
 os.system('mkdir %s%s/wc2' % (outdir,prefix))
@@ -63,7 +63,7 @@ for ff,fname in enumerate(wc2files):
     if int(variable) in range(1,20):
         wc2vars.append(variable)
         wc2subset.append(fname)
-        os.system("gdalwarp -overwrite -te %f %f %f %f -of -dstnodata -9999  %s %s%s/wc2/%s_%s.tif" % (W,S,E,N,wc2files[ff],outdir,prefix,outfname,prefix))
+        os.system('gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999  %s %s%s/wc2/%s_%s.tif' % (W,S,E,N,wc2files[ff],outdir,prefix,outfname,prefix))
 
 # Soilgrids next. Soilgrids are downloaded as geotiffs. Resolution is identical to worldclim, so regridding process is the same
 sgdir = '/disk/scratch/local.2/soilgrids/1km/'
@@ -75,13 +75,13 @@ for ff,fname in enumerate(sgfiles):
     outfname = fname.split('/')[-1][:-4]
     sgvars.append(variable)
     sgsubset.append(fname)
-    os.system("gdalwarp -overwrite -te %f %f %f %f %s %s%s/soilgrids/%s_%s.tif" % (W,S,E,N,sgfiles[ff],outdir,prefix,outfname,prefix))
+    os.system('gdalwarp -overwrite -te %f %f %f %f %s %s%s/soilgrids/%s_%s.tif' % (W,S,E,N,sgfiles[ff],outdir,prefix,outfname,prefix))
 
 # Aboveground Biomass - Avitabile map - 1 km resolution so same gdal example should be sufficient
 agbfiles= ['/home/dmilodow/DataStore_GCEL/AGB/avitabile/Avitabile_AGB_Map/Avitabile_AGB_Map.tif','/home/dmilodow/DataStore_GCEL/AGB/avitabile/Avitabile_AGB_Uncertainty/Avitabile_AGB_Uncertainty.tif']
 agbvars = ['Avitabile_AGB','Avitabile_AGB_Uncertainty']
-os.system("gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999 %s %s%s/agb/%s_%s_1km.tif" % (W,S,E,N,agbfiles[0],outdir,prefix,agbvars[0],prefix))
-os.system("gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999  %s %s%s/agb/%s_%s_1km.tif" % (W,S,E,N,agbfiles[1],outdir,prefix,agbvars[1],prefix))
+os.system('gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999 %s %s%s/agb/%s_%s_1km.tif' % (W,S,E,N,agbfiles[0],outdir,prefix,agbvars[0],prefix))
+os.system('gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999  %s %s%s/agb/%s_%s_1km.tif' % (W,S,E,N,agbfiles[1],outdir,prefix,agbvars[1],prefix))
 
 # ESA CCI landcover - the original resolution of these rasters is higher than the reference data, so this needs to be regridded. The mode landcover class will be used.
 # Note that the ESA CCI landcover data are originally in netcdf format.
@@ -89,25 +89,25 @@ lcdir = '/home/dmilodow/DataStore_GCEL/ESA_CCI_landcover/'
 lcfiles = glob.glob('%s*.nc' % lcdir);lcfiles.sort()
 outfiles = glob.glob('%s*1km*.tif' % lcdir);lcfiles.sort()
 for ff,fname in enumerate(lcfiles):
-    outfname = outfiles[ff].replace('1km','500m')
-    os.system("gdalwarp -overwrite -te %f %f %f %f  -tr 0.004166666666667 -0.004166666666667 -r mode -of GTIFF NETCDF:%s:lccs_class %s/esacci/%s" % (W,S,E,N,lcfiles[ff],outdir,outfname))
-    outfname = fname.split('/')[-1][:22]+fname.split('/')[-1][27:42]+"-1km-mode-lccs-class"
-    os.system("gdalwarp -overwrite -te %f %f %f %f  -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF NETCDF:%s:lccs_class %s%s/esacci/%s-%s.tif" % (W,S,E,N,lcfiles[ff],outdir,prefix,outfname,prefix))
-    outfname = fname.split('/')[-1][:22]+fname.split('/')[-1][27:42]+"-1km-mode-change-count"
-    os.system("gdalwarp -overwrite -te %f %f %f %f -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF NETCDF:%s:change_count %s%s/esacci/%s-%s.tif" % (W,S,E,N,lcfiles[ff],outdir,prefix,outfname,prefix))
+    #outfname = outfiles[ff].replace('1km','500m')
+    #os.system('gdalwarp -overwrite -te %f %f %f %f  -tr 0.004166666666667 -0.004166666666667 -r mode -of GTIFF NETCDF:%s:lccs_class %s/esacci/%s' % (W,S,E,N,lcfiles[ff],outdir,outfname))
+    outfname = fname.split('/')[-1][:22]+fname.split('/')[-1][27:42]+'-1km-mode-lccs-class'
+    os.system('gdalwarp -overwrite -te %f %f %f %f  -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF NETCDF:%s:lccs_class %s%s/esacci/%s-%s.tif' % (W,S,E,N,lcfiles[ff],outdir,prefix,outfname,prefix))
+    outfname = fname.split('/')[-1][:22]+fname.split('/')[-1][27:42]+'-1km-mode-change-count'
+    os.system('gdalwarp -overwrite -te %f %f %f %f -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF NETCDF:%s:change_count %s%s/esacci/%s-%s.tif' % (W,S,E,N,lcfiles[ff],outdir,prefix,outfname,prefix))
 
 
 lcdir = './'
 lcfiles = glob.glob('%s*.nc' % lcdir);lcfiles.sort()
 for ff,fname in enumerate(lcfiles):
-    outfname = fname.split('/')[-1][:22]+"1km-"+fname.split('/')[-1][27:42]+"-mode-lccs-class"
-    os.system("gdalwarp -overwrite -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF NETCDF:%s:lccs_class ./%s.tif" % (lcfiles[ff],outfname))
+    outfname = fname.split('/')[-1][:22]+'1km-'+fname.split('/')[-1][27:42]+'-mode-lccs-class'
+    os.system('gdalwarp -overwrite -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF NETCDF:%s:lccs_class ./%s.tif' % (lcfiles[ff],outfname))
 
 
 # Primary forest cover and loss (Margono et al.)
 f_file = '/home/dmilodow/DataStore_DTM/FOREST2020/PartnerCountries/Indonesia/ForestCover/Primary_and_intact_forest_and_loss/change/margono_indonesia_forestcover_and_loss_1km.tif'
-os.system("gdalwarp -overwrite -te %f %f %f %f  -dstnodata -9999 -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF %s %s%s/forestcover/margono_indonesia_forestcover_and_loss.tif" % (W,S,E,N,f_file,outdir,prefix))
+os.system('gdalwarp -overwrite -te %f %f %f %f  -dstnodata -9999 -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF %s %s%s/forestcover/margono_indonesia_forestcover_and_loss.tif' % (W,S,E,N,f_file,outdir,prefix))
 
 # INtact forest landscapes (... et al.)
-hfl_file = '/disk/scratch/local.2/global_hinterland_forests/source_files/SAM_2013hinterland_25vcf.tif'
-os.system("gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999  -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF %s %s%s/forestcover/HFL_2013_%s.tif" % (W,S,E,N,hfl_file,outdir,prefix,prefix))
+hfl_file = '/disk/scratch/local.2/global_hinterland_forests/source_files/SEA_2013hinterland_25vcf.tif'
+os.system('gdalwarp -overwrite -te %f %f %f %f -dstnodata -9999  -tr 0.008333333333333 -0.008333333333333 -r mode -of GTIFF %s %s%s/forestcover/HFL_2013_%s.tif' % (W,S,E,N,hfl_file,outdir,prefix,prefix))
